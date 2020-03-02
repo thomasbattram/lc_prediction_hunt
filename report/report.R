02d8dadf147b04e4b9d6745a752800cd879365cc
Thomas Battram <tb13101@bristol.ac.uk>
2020-03-02 07:48:27 +0000

    updated

--- report/report.R
+++ report/report.R
@@ -15,18 +15,13 @@ load(paste0(dir, "/roc_dat.RData"))
 res <- list(roc_res$all$roc_dat, roc_res$ahrr$roc_dat)
 names(res) <- c("all_cpgs", "cg05575921 (AHRR)")
 ## why doesn't this work!!!
-p_all <- pROC::ggroc(res) +
+p_both <- pROC::ggroc(res) +
 	geom_abline(intercept = 1, slope = 1, colour = "black", alpha = 0.6) +
-	annotate("text", x = 0.7, y = 0.9, label = roc_res$all$plot_text) +
-	annotate("text", x = 0.7, y = 0.85, label = roc_res$ahrr$plot_text) +
-	theme_bw()
-	# theme(legend.position = "none")
-
-p_ahrr <- pROC::ggroc(list(roc_res$ahrr$roc_dat)) +
-	geom_abline(intercept = 1, slope = 1, colour = "black", alpha = 0.6) +
+	annotate("text", x = 0.7, y = 0.95, label = roc_res$all$plot_text) +
 	annotate("text", x = 0.7, y = 0.9, label = roc_res$ahrr$plot_text) +
 	theme_bw() +
-	theme(legend.position = "none")
+	labs(colour = NULL)
+	# theme(legend.position = "none")
 
 ## ---- roc_plot --------------------------------
-print(p)
+print(p_both)
