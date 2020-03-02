4e3b952cf62ec9f00f6527aadf7b507ec1af1210
Thomas Battram <tb13101@bristol.ac.uk>
2020-03-02 10:32:37 +0000

    updated to include more cpg sets

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -2,11 +2,11 @@
 # Predicting lung cancer in NSHDS with DNA methylation data
 # --------------------------------------------
 
-# 76 CpGs were identified in an EWAS of lung cancer in HUNT and the authors
-# are looking to test how well the predict lung cancer in an independent
-# dataset --> NSHDS.
+# CpGs were identified in an EWAS of smoking, smoking status change
+# and lung cancer in HUNT and the authors are looking to test how well
+# the CpGs predict lung cancer in an independent dataset --> NSHDS. 
 
-pkgs <- c("tidyverse", "survival", "pROC")
+pkgs <- c("tidyverse", "survival", "pROC", "readxl")
 lapply(pkgs, require, character.only = TRUE)
 
 devtools::load_all("~/repos/usefunc/")
@@ -15,8 +15,10 @@ devtools::load_all("~/repos/usefunc/")
 data_file <- "data/cleaned_pheno_and_meth_data.RData"
 load(data_file)
 
-# read in 76 CpGs
-int_cpgs <- read_csv("data/76_CpGs_HUNT_YQS.csv", skip=1)
+# read in CpGs of interest
+smok_ewas <- read_xlsx("data/hunt_ewas_data.xlsx", sheet = 1)
+smok_stat_ewas <- read_xlsx("data/hunt_ewas_data.xlsx", sheet = 2)
+lc_ewas <- read_xlsx("data/hunt_ewas_data.xlsx", sheet = 3)
 
 meth_data_file <- "~/tb_MB009_LungCancer/RDATA/FunctionalNormalised_Betas_PvaluePassed_Samples.RData"
 load(meth_data_file) 
@@ -26,36 +28,94 @@ load(meth_data_file)
 # extract variables needed from nshds data
 # -----------------------------------
 
-meth_res <- out$meth %>%
-	dplyr::filter(cpg %in% int_cpgs[["CPG Labels"]])
-# only 41 left...
-which(!int_cpgs[["CPG Labels"]] %in% out$meth$cpg)
+ewas <- list(smoking = smok_ewas, 
+			 smoking_status = smok_stat_ewas, 
+			 lung_cancer = lc_ewas)
+
+cpgs <- list(smoking = smok_ewas$cpg, 
+			 smoking_status = smok_stat_ewas$cpg, 
+			 lung_cancer = lc_ewas$cpg)
+
+unique_cpgs <- unique(unlist(cpgs))
+
+meth_res <- lapply(1:3, function(x) {
+	cpg_list <- cpgs[[x]]
+	dat <- out$meth %>%
+		dplyr::filter(cpg %in% cpg_list)
+	return(dat)
+})
+names(meth_res) <- names(cpgs)
+
+# only 51 left...
+which(!unique_cpgs %in% out$meth$cpg)
+
+# how many are there in each cpg dataset
+
+# ewas n_cpgs n_cpgs_in450k unique_cpgs_in450k
+#
+#
+x=1
+cpg_dat_sum <- map_dfr(1:3, function(x) {
+	cpg_list <- cpgs[[x]]
+	other_cpg_lists <- unique(unlist(cpgs[-x]))
+	ewas_nam <- names(cpgs[x])
+	ncpgs <- length(cpg_list)
+	list_450k <- cpg_list[cpg_list %in% out$meth$cpg]
+	list_unique <- list_450k[!list_450k %in% other_cpg_lists]
+	out <- data.frame(
+		ewas = ewas_nam, 
+		n_cpgs = ncpgs, 
+		n_cpgs_in_450k = length(list_450k), 
+		n_unique_cpgs_in_450k = length(list_unique)
+		)
+})
+
+overall <- data.frame(
+			ewas = "combined", 
+			n_cpgs = length(unlist(cpgs)), 
+			n_cpgs_in_450k = sum(unique_cpgs %in% out$meth$cpg), 
+			n_unique_cpgs_in_450k = sum(unique_cpgs %in% out$meth$cpg)
+			)
+cpg_dat_sum <- bind_rows(list(cpg_dat_sum, overall))
+
+write.table(cpg_dat_sum, "report/report_data/ewas_dat_summary.txt")
+
 # aries check
 # load("/panfs/panasas01/sscm/ms13525/aries-release-v4/data/betas/data.Robj")
 # sum(rownames(beta) %in% int_cpgs[["CPG Labels"]]) # 41
 #
 
-# only 41 because they used EPIC array data!!!!
+# only 51 because they used EPIC array data!!!!
 
 # derive DNAm score for each individual
-rownames(meth_res) <- meth_res$cpg
-meth_res2 <- t(meth_res)[-1, ]
-meth_res2 <- meth_res2 %>%
-	as.data.frame %>%
-	rownames_to_column(var = "sample")
+lc_ewas <- lc_ewas %>%
+	mutate(Coefficient = log(ORb))
 
-fin_res <- map_dfc(1:ncol(meth_res2), function(x) {
-	col_nam <- colnames(meth_res2)[x]
-	if (col_nam == "sample") return(meth_res2[[x]])
-	
-	vals <- as.numeric(as.character(meth_res2[[x]]))
-	coef <- int_cpgs %>%
-		dplyr::filter(`CPG Labels` == col_nam) %>%
-		pull(Coefficient)
-	out <- coef * vals
-	return(out)
+meth_res2 <- lapply(1:3, function(x) {
+	# orient the data
+	dat <- meth_res[[x]]
+	rownames(dat) <- dat$cpg
+	dat2 <- t(dat)[-1, ]
+	dat2 <- dat2 %>%
+		as.data.frame %>%
+		rownames_to_column(var = "sample")
+
+	ewas_dat <- ewas[[x]]
+	fin_res <- map_dfc(1:ncol(dat2), function(x) {
+		col_nam <- colnames(dat2)[x]
+		if (col_nam == "sample") return(dat2[[x]])
+		
+		vals <- as.numeric(as.character(dat2[[x]]))
+		coef <- ewas_dat %>%
+			dplyr::filter(cpg == col_nam) %>%
+			pull(Coefficient)
+		out <- coef * vals
+		return(out)
+	})
+	colnames(fin_res) <- colnames(dat2)
+	return(fin_res)
 })
-colnames(fin_res) <- colnames(meth_res2)
+names(meth_res2) <- names(cpgs)
 
 
 # checking smoking variables to find pack years!
@@ -65,8 +125,8 @@ grep("SMOK", colnames(out$pheno), value = TRUE)
 # to be sure
 
 phen_res <- out$pheno %>%
-	dplyr::select(sentrix, CASESET, LUNG_CANCER_CASE, DOB) %>%
-	left_join(fin_res, by = c("sentrix" = "sample"))
+	dplyr::select(sentrix, CASESET, LUNG_CANCER_CASE, DOB) # %>%
+	# left_join(fin_res, by = c("sentrix" = "sample"))
 
 # -----------------------------------
 # run the analysis
@@ -99,40 +159,38 @@ roc.test(ROC.base, ROC.plus.cpg.nodob)
 # ; 3: former ≥20.1 pyrs; 4: current ≤10.0 pyrs; 5: current 10.1-20.0 pyrs;
 # and 6: current ≥20.1 pyrs].
 
-cpg_sites <- grep("cg", colnames(phen_res), value = TRUE)
-vars1 <- paste(paste(cpg_sites, collapse = " + "), "strata(CASESET)", sep = " + ")
-form1 <- as.formula(paste0("LUNG_CANCER_CASE ~ ", vars1))
-fit1 <- clogit(form1, data = phen_res)
+cpgs <- c(cpgs, list(ahrr = "cg05575921"))
 
-ahrr_cpg <- "cg05575921"
-vars2 <- paste(paste(ahrr_cpg, collapse = " + "), "strata(CASESET)", sep = " + ")
-form2 <- as.formula(paste0("LUNG_CANCER_CASE ~ ", vars2))
-fit2 <- clogit(form2, data = phen_res)
-
-roc_all <- roc(phen_res$LUNG_CANCER_CASE, fit1$linear.predictors, ci = T)
-roc_ahrr <-roc(phen_res$LUNG_CANCER_CASE, fit2$linear.predictors, ci = T)
-res <- roc.test(roc_all, roc_ahrr)
-
-roc_list <- list(all = roc_all, ahrr = roc_ahrr)
-i=1
-roc_res <- lapply(1:2, function(i) {
-	roc_dat <- roc_list[[i]]
-	if (names(roc_list[i]) == "ahrr") {
-		cpg_name <- ahrr_cpg
+all_res <- lapply(1:length(cpgs), function(x) {
+	print(x)
+	ewas_nam <- names(cpgs)[x]
+	if (x == 4) {
+		meth_dat <- meth_res2[[1]] %>%
+			dplyr::select(sample, cg05575921)
 	} else {
-		cpg_name <- "all_cpgs"
+		meth_dat <- meth_res2[[ewas_nam]]
 	}
-	auc_dat <- t(as.data.frame(ci.auc(roc_dat))) %>%
+	phen_dat <- phen_res %>%
+		left_join(meth_dat, by = c("sentrix" = "sample"))
+	cpg_sites <- grep("cg", colnames(phen_dat), value = TRUE)
+	# model
+	vars <- paste(paste(cpg_sites, collapse = " + "), "strata(CASESET)", sep = " + ")
+	form <- as.formula(paste0("LUNG_CANCER_CASE ~ ", vars))
+	fit <- clogit(form, data = phen_dat)
+	# roc curve
+	roc_res <- roc(phen_dat$LUNG_CANCER_CASE, fit$linear.predictors, ci = T)
+	# auc
+	auc_dat <- t(as.data.frame(ci.auc(roc_res))) %>%
 		as.data.frame
 	rownames(auc_dat) <- NULL
 	colnames(auc_dat) <- c("lower", "estimate", "upper")
-	plot_text <- paste0(cpg_name, ": ", comma(auc_dat$estimate), " (95% CI: ",
+	plot_text <- paste0(ewas_nam, ": ", comma(auc_dat$estimate), " (95% CI: ",
 					comma(auc_dat$lower), " - ", comma(auc_dat$upper), ")")
-	return(list(roc_dat = roc_dat, auc = auc_dat, plot_text = plot_text))
+	return(list(roc_dat = roc_res, auc = auc_dat, plot_text = plot_text))
 })
-names(roc_res) <- names(roc_list)
+names(all_res) <- names(cpgs)
 
-save(roc_res, file = "report/report_data/roc_dat.RData")
+save(all_res, file = "report/report_data/roc_dat.RData")
 
 p <- pROC::ggroc(list(roc_res$ahrr$roc_dat)) +
 	geom_abline(intercept = 1, slope = 1, colour = "black", alpha = 0.6) +
@@ -140,16 +198,3 @@ p <- pROC::ggroc(list(roc_res$ahrr$roc_dat)) +
 	theme_bw() +
 	theme(legend.position = "none")
 ggsave("results/roc_plot.pdf", plot = p)
-
-
-
-# models
-GLM <- glm(Y ~. , data = X.base, family = binomial(logit))
-# Y is LC variable (0, 1); X.base is data.frame including variables sex, age and smoking (7-levels score)
-ROC.base <- roc(GLM$y, GLM$fitted.values, ci = T)
-GLM <- glm(Y ~. , data = X.plus.76, family = binomial(logit))
-# Y is LC variable (0, 1); X.plus.76 is data.frame including variables sex, age, smoking (7-levels score) and 76 CpGs
-ROC.plus.76 <-roc(GLM$y, GLM$fitted.values, ci = T)
-# Compare 2 ROCs:
-roc.test(ROC.base, ROC.plus.76)
-
