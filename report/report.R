b35a3108f4dd0f663cdc41cfd4c2019aedac6e12
Thomas Battram <tb13101@bristol.ac.uk>
2020-03-09 11:52:49 +0000

    Updated report

--- report/report.R
+++ report/report.R
@@ -10,7 +10,7 @@ qc_sum <- read_tsv(paste0(dir, "qc_summary.txt"))
 ewas_sum <- read_tsv(paste0(dir, "ewas_dat_summary.txt"))
 
 # roc results
-load(paste0(dir, "/roc_dat.RData"))
+load(paste0(dir, "roc_dat.RData"))
 
 # fix this in previous scripts!!
 names(all_res)[2] <- "smoking_status_change"
@@ -21,31 +21,45 @@ pandoc.table(ewas_sum)
 
 ## ---- roc_setup --------------------------------
 x=1
-roc_res <- lapply(1:length(all_res), function(x) {
-	res_nam <- names(all_res)[x]
-	all_res[[x]]$roc_dat
+
+model_type <- c("separate_sites", "score")
+roc_res <- lapply(1:length(model_type), function(x) {
+	res <- map(all_res, model_type[x])
+	out_res <- lapply(1:length(res), function(i) {
+		res[[i]]$roc_dat
+	})
+	names(out_res) <- names(res)
+	return(out_res)
 })
-names(roc_res) <- names(all_res)
+names(roc_res) <- model_type
 
 # generate AUC tables
 auc_res <- map_dfr(1:length(all_res), function(x) {
 	res_nam <- names(all_res)[x]
-	auc_dat <- all_res[[res_nam]]$auc %>%
-		mutate(cpg_set = res_nam) %>%
-		dplyr::select(cpg_set, estimate, lower, upper) %>%
-		rename(auc = estimate, ci_lower = lower, ci_upper = upper)
-	return(auc_dat)
+	out <- map_dfr(1:length(model_type), function(i) {
+		auc_dat <- all_res[[res_nam]][[model_type[i]]]$auc %>%
+			mutate(cpg_set = res_nam) %>%
+			mutate(model = model_type[i]) %>%
+			dplyr::select(cpg_set, model, estimate, lower, upper) %>%
+			rename(auc = estimate, ci_lower = lower, ci_upper = upper)
+	})
+	return(out)
 })
 
-## why doesn't this work!!!
-p <- pROC::ggroc(roc_res) +
-	geom_abline(intercept = 1, slope = 1, colour = "black", alpha = 0.6) +
-	theme_bw() +
-	labs(colour = NULL)
-	# theme(legend.position = "none")
+p <- lapply(1:length(model_type), function(x) {
+	model <- model_type[x]
+	out <- pROC::ggroc(roc_res[[model]]) +
+		geom_abline(intercept = 1, slope = 1, colour = "black", alpha = 0.6) +
+		theme_bw() +
+		labs(colour = NULL)
+})
+names(p) <- model_type
 
-## ---- roc_plot --------------------------------
-print(p)
+## ---- roc_plot_separate_sites --------------------------------
+print(p$separate_sites)
+
+## ---- roc_plot_score --------------------------------
+print(p$score)
 
 ## ---- auc_tab --------------------------------
 pandoc.table(auc_res)
